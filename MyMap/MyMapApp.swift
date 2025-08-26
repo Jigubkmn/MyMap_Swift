889a4d26b6b02af4281deaec861440b1d88cb11c
Masashi Shiomi <bkmn.31519@gmail.com>
2025-08-26 15:44:41 +0900

    Initial Commit

---
+++ MyMap/MyMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMapApp.swift
+//  MyMap
+//
+//  Created by 塩見昌士 on 2025/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MyMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
